d0427b972368c81b2ee0633d17b864613088d865
Michelle Olivieri <michelleo72@gmail.com>
2018-04-30 16:14:32 +0000

    automatically backed up by learn

--- db/migrate/20180430161411_add_columns_to_movies.rb
+++ db/migrate/20180430161411_add_columns_to_movies.rb
@@ -1,4 +1,11 @@
 class AddColumnsToMovies < ActiveRecord::Migration
+
   def change
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :datetime
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
   end
+  
 end
